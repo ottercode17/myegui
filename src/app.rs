382a572c24546a0318c665c3f4477ac7861ef4db
ottercode17 <153008411+ottercode17@users.noreply.github.com>
2025-06-19 10:38:15 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -70,7 +70,7 @@ impl eframe::App for TemplateApp {
             ui.heading("eframe template");
 
             ui.horizontal(|ui| {
-                ui.label("Write somethingyyy: ");
+                ui.label("Write somethingyyy17: ");
                 ui.text_edit_singleline(&mut self.label);
             });
 
