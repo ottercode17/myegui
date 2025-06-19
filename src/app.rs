da3e4b1d88091011473721098a7bf8a795964191
ottercode17 <153008411+ottercode17@users.noreply.github.com>
2025-06-19 11:31:48 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -70,7 +70,7 @@ impl eframe::App for TemplateApp {
             ui.heading("eframe template");
 
             ui.horizontal(|ui| {
-                ui.label("Write somethingWOODLAND2026: ");
+                ui.label("Write somethingWOODLAND202617: ");
                 ui.text_edit_singleline(&mut self.label);
             });
 
