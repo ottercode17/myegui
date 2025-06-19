cd1b8d9de226cf02dbcfaf2448460fa4b02fea5d
ottercode17 <153008411+ottercode17@users.noreply.github.com>
2025-06-19 12:47:11 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -70,7 +70,7 @@ impl eframe::App for TemplateApp {
             ui.heading("eframe template");
 
             ui.horizontal(|ui| {
-                ui.label("Write somethingWOODLANDwedding2026kimashi17: ");
+                ui.label("Write somethingWOODLANDotters: ");
                 ui.text_edit_singleline(&mut self.label);
             });
 
